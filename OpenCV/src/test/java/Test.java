eeab8a6b3c208773e96b8ab36071967bebb44640
FuYY <360687243@qq.com>
2020-03-19 19:26:05 +0800

    BUG修复

--- OpenCV/src/test/java/Test.java
+++ OpenCV/src/test/java/Test.java
@@ -1,3 +1,4 @@
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfFloat;
 import org.opencv.core.MatOfInt;
@@ -7,6 +8,10 @@ import org.opencv.imgproc.Imgproc;
 import java.util.Arrays;
 
 public class Test {
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
     public static double compare_image(String img_1, String img_2) {
         Mat mat_1 = Imgcodecs.imread(img_1);
         Mat mat_2 = Imgcodecs.imread(img_2);
@@ -22,18 +27,17 @@ public class Test {
         Imgproc.calcHist(Arrays.asList(mat_2), new MatOfInt(0), new Mat(), hist_2, histSize, ranges);
 
         // CORREL 相关系数
-        double res = Imgproc.compareHist(hist_1, hist_2, Imgproc.CV_COMP_CORREL);
-        return res;
+        return Imgproc.compareHist(hist_1, hist_2, Imgproc.CV_COMP_CORREL);
     }
 
     public static void main(String[] args) {
-        String basePicPath = "F:\\workTest\\FaceOpenCVData\\";
-        double compareHist = compare_image(basePicPath + "1.jpg", basePicPath + "000.jpg");
+        String basePicPath = "E:\\fuyy\\AiFuWaiWaiGongJuBao\\OpenCV\\src\\main\\resources\\image\\";
+        double compareHist = compare_image(basePicPath + "1.jpg", basePicPath + "3.jpg");
         System.out.println(compareHist);
         if (compareHist > 0.72) {
-            System.out.println("人脸匹配");
+            System.out.println("匹配");
         } else {
-            System.out.println("人脸不匹配");
+            System.out.println("不匹配");
         }
     }
 }
