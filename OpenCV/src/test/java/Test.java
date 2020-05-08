d5bb9189d47ded6d5ea9610054ed8d2f92bc8157
FYY <360687243@qq.com>
2020-05-08 23:48:30 +0800

    配置方式

--- OpenCV/src/test/java/Test.java
+++ OpenCV/src/test/java/Test.java
@@ -14,6 +14,18 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+
+//E:\JetBrains\IntelliJ IDEA\plugins\maven\lib\maven3\bin> mvn install:install-file -DgroupId=com.opencv -DartifactId=opencv -Dversion=4.2.0 -Dpackaging=jar -Dfile=E:\OpenCV\opencv\build\java\opencv-420.jar
+
+//-ea -Djava.library.path=E:\OpenCV\opencv\build\java\x64 -Djava.awt.headless=false
+
+//<!-- OpenCV -->
+//<dependency>
+//<groupId>com.opencv</groupId>
+//<artifactId>opencv</artifactId>
+//<version>4.2.0</version>
+//</dependency>
+
 public class Test {
     private static String basePicPath = "D:\\Project\\FYY\\AiFuWaiWaiGongJuBao\\OpenCV\\src\\main\\resources\\image\\";
 
@@ -23,31 +35,31 @@ public class Test {
 
         System.out.println(DateDispose.formatting_Date(sd, DateType.Year_Month_Day_Hour_Minute_Second_MS));
 
-//        CalcHistCompare calcHistCompare = CalcHistCompare.create()
-//                .setCalcHistSize(10)
-//                .setThreshold(0.993)
-//                .build();
-//
-//        LinkedHashMap<String, Double> map = calcHistCompare.compare(basePicPath + "24158.jpg", basePicPath);
-//
-//        for (Map.Entry<String, Double> entry : map.entrySet()) {
-//            System.out.println("图片：" + entry.getKey() + " 对比值：" + entry.getValue());
-//        }
-
-//        orb();
-
-        ORBSearch orbSearch = ORBSearch.create()
+        CalcHistCompare calcHistCompare = CalcHistCompare.create()
+                .setCalcHistSize(10)
+                .setThreshold(0.993)
                 .build();
 
-        LinkedHashMap<String, Double> map = orbSearch.search(basePicPath + "24158.jpg", basePicPath);
+        LinkedHashMap<String, Double> map = calcHistCompare.compare(basePicPath + "24158.jpg", basePicPath);
 
         for (Map.Entry<String, Double> entry : map.entrySet()) {
             System.out.println("图片：" + entry.getKey() + " 对比值：" + entry.getValue());
         }
 
-        Date ed = new Date();
-        System.out.println(DateDispose.formatting_Date(ed, DateType.Year_Month_Day_Hour_Minute_Second_MS));
-        System.out.println("执行时间为：" + (sd.getTime() - ed.getTime()) + "毫秒");
+//        orb();
+
+//        ORBSearch orbSearch = ORBSearch.create()
+//                .build();
+//
+//        LinkedHashMap<String, Double> map = orbSearch.search(basePicPath + "24158.jpg", basePicPath);
+//
+//        for (Map.Entry<String, Double> entry : map.entrySet()) {
+//            System.out.println("图片：" + entry.getKey() + " 对比值：" + entry.getValue());
+//        }
+//
+//        Date ed = new Date();
+//        System.out.println(DateDispose.formatting_Date(ed, DateType.Year_Month_Day_Hour_Minute_Second_MS));
+//        System.out.println("执行时间为：" + (sd.getTime() - ed.getTime()) + "毫秒");
 
 //        double compareHist = compare_image(basePicPath + "1.jpg", basePicPath + "3.jpg");
 //        System.out.println(compareHist);
